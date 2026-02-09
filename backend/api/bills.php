b3661816f790859968f2542e35c7ae7ee660ee82
Yumegaarux <zaldarriagajanvictor@gmail.com>
2026-02-09 12:22:01 +0800

    displaying of senate bills now working with displaying of title, number, authors.

--- backend/api/bills.php
+++ backend/api/bills.php
@@ -2,7 +2,7 @@
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 
-$url = "https://open-congress-api.bettergov.ph/api/documents";
+$url = "https://open-congress-api.bettergov.ph/api/documents?limit=20&congress=20&type=SB";
 
 $response = file_get_contents($url);
 
--- frontend/pages/Test.jsx
+++ frontend/pages/Test.jsx
@@ -4,11 +4,15 @@ import axios from "axios";
 function Test() {
     const [bills, setBills] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [cursor, setCursor] = useState(null);
+    const [hasMore, setHasMore] = useState(true);
+
 
     useEffect(() => {
-        axios.get("http://localhost/billreviewer/billreviewer/backend/api/bills.php")
+        axios.get("http://localhost/billreviewer/billreviewer/backend/api/bills.php?limit=20&congress=20&type=SB")
             .then(res => {
-            setBills(res.data.data);
+                setBills(res.data.data);
+                setLoading(false);
             });
     }, []);
 
@@ -20,8 +24,22 @@ function Test() {
 
             {bills.map(bill => (
                 <div key={bill.id}>
-                    <h3>{bill.title}</h3>
+                    <h2>Bill Title: {bill.long_title || "No Available Title."}</h2>
+                    <h2>Bill No. {bill.name}</h2>
+                    <h3>Date Filed: {bill.date_filed}</h3>
                     <p>Status: {bill.status}</p>
+                    <p>Subtype: {bill.subtype}</p>
+
+                    {bill.authors.length > 0 ? (
+                        bill.authors.map((author) => (
+                            <div key={author.id}>
+                                <p>Authors: {author.first_name + ' ' + author.middle_name + ' ' + author.last_name}</p>
+                            </div>
+                        )) 
+                    ) : (
+                        <p>Authors: No Authors Listed</p>
+                    )}
+                    
                 </div>
             ))}
         </>

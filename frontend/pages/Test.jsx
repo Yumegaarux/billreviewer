03f02060ad1f206601bf88327ecde62acc1459db
Yumegaarux <zaldarriagajanvictor@gmail.com>
2026-02-28 18:43:01 +0800

    fix: pagination with searching now works.

--- frontend/pages/Test.jsx
+++ frontend/pages/Test.jsx
@@ -4,70 +4,81 @@ import { API_BASE_URL } from "../util/api";
 
 function Test() {
     const [bills, setBills] = useState([]);
+    const [total, setTotal] = useState(0);
     const [page, setPage] = useState(1);
     const [loading, setLoading] = useState(true);
     const [offset, setOffset] = useState(0);
     const [hasMore, setHasMore] = useState(true);
     const [filter, setFilter] = useState("");
     const [searchVal, setSearchVal] = useState("");
-    const effectRan = useRef(false);
+    const [backActive, setBackActive] = useState(false);
 
     function buttonNavigate(buttonPagination) {
-        const calculatedOffset = buttonPagination * 20;
+        const calculatedOffset = (buttonPagination - 1) * 20;
         console.log("Current Calculated Offset: ", calculatedOffset);
 
         fetchBills(calculatedOffset);
     }
 
     function createPagination() {
+        console.log("Pagination Created!");
         let buttonPagination = [];
 
-        for (let i = page ; i <= page + 10; i++) { 
+        const totalPages = Math.ceil(total / 20); // 20 per page
+
+        for (let i = 1; i <= totalPages; i++) {
             buttonPagination.push(
-                <button onClick={() => buttonNavigate(i, setPage(i))}
-                    key={i}>{i}
-                 </button>
+                <button 
+                    onClick={() => {
+                        setPage(i);
+                        buttonNavigate(i);
+                    }}
+                    key={i}
+                >
+                    {i}
+                </button>
             );
         }
 
         return buttonPagination;
     }
 
+    function handleBack() {
+        console.log("handleBack active");
+        setBackActive(true);
+        setLoading(true);
+        setPage(1);
+        setOffset(0);
+        setSearchVal("");
+
+        const searchBar = document.querySelectorAll("searchBar");
+        searchBar.value = "";
+
+        setLoading(false);
+    }
+
     // async functions allow the user to do other functions until 'await' is finished.
     // it doesn't stuck the whole system up freezing waiting for a function to finish.
-    const handleSearch = async (event) => { // event is usually used when trying to use an 'inputs' value for it's onChange function
-        try {
-            setLoading(true);
-
-            console.log("Fetched Keyword: ", searchVal);
-
-            const res = await axios.get(`${API_BASE_URL}/search/documents`, {
-                params: {
-                    q: searchVal,
-                }
-            });
-            // 'axios' converts 'params' into query string parameters like '?filter=title&keyword='block' 
-            console.log("Handle Search Running...");
-            console.log("Axios Get: ", `${API_BASE_URL}/search/documents?q=${searchVal}`);
-
-            const newBills = Array.isArray(res.data.data) ? res.data.data : [];
-            console.log("New Billz: ", newBills);
-
-            setBills(newBills);
-            setLoading(false);
-        } catch (err) {
-            console.log(err);
-        }    
-    }
+    // event is usually used when trying to use an 'inputs' value for it's onChange function
 
     const fetchBills = async (offset = 0) => {
         setLoading(true);
         try {
             console.log("Fetching with offset:", offset);
-            const res = await axios.get(
-                // Using offset based pagination because API was sending cursor=20 by default which is wrong. 
-                `${API_BASE_URL}?offset=${offset}`
-            );  
+            let res;
+            if (searchVal === '') {
+                res = await axios.get(
+                    // Using offset based pagination because API was sending cursor=20 by default which is wrong. 
+                    `${API_BASE_URL}?offset=${offset}`
+                );  
+            } else {
+                res = await axios.get(`${API_BASE_URL}/search/documents`, {
+                    params: {
+                        q: searchVal,
+                        'offset': offset,
+                    }
+                });
+            }
 
             // DEBUG
             console.log("API Response:", res.data);
@@ -83,19 +94,17 @@ function Test() {
             setOffset(offset);
             // API returns if this pagination still has more.
             setHasMore(res.data.pagination?.has_more || false);
+            setBackActive(false);
+
         } catch (err) {
             console.error(err);
         }
         setLoading(false);
     };
 
-    function clearSearch(){
-        
-    }
-
     useEffect(() => {
         const delayDebounce = setTimeout(() => {
-            searchVal === "" ? fetchBills() : handleSearch(searchVal);
+            fetchBills();
         }, 425)
 
         return () => clearTimeout(delayDebounce);
@@ -104,7 +113,7 @@ function Test() {
 
     return (
         <>
-            <button onClick={fetchBills}>Back</button>
+            <button onClick={handleBack}>Back</button>
             <input type="text" value = {searchVal} onChange={(e) => setSearchVal(e.target.value)} className="searchBar"></input>
 {/* 
                 <input type="radio" id="Title" name="filter"  onChange={() => setFilter("Title")}></input>

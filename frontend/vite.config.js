d3998247048c13fdfc0867e67514189629d13296
Yumegaarux <zaldarriagajanvictor@gmail.com>
2026-03-14 10:55:33 +0800

    refactor: backend and frontend containers finally running.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: 'frontend',
   plugins: [react()],
+  server: {
+    port: 9000,
+    host: '0.0.0.0',  
+  },
 })
